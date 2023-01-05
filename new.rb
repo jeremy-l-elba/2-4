060c3f63709dd45c2c648cdd5f4d34af91921646
jeremy-l-elba <120450905+jeremy-l-elba@users.noreply.github.com>
2023-01-05 12:02:20 +0000

    2-8

---
+++ new.rb
@@ -0,0 +1,15 @@
+class Car
+  def self .turn(direction)
+    puts "#{direction}に曲がります。"
+  end
+
+  def run(distance)
+    puts "車で#{distance}キロ走ります。"
+  end
+end
+
+
+Car.turn("右")
+
+car = Car.new
+car.run(5)
